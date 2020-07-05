fd43234ce3b40b6aa9e0cca71b341214a64684e8
Grace.W <55606704+XiaofanWang1010@users.noreply.github.com>
2020-07-05 01:53:12 -0400

    Add files via upload

---
+++ 9-Palindrome Number-Math.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        if x<0:
+            return False
+        str_1 = str(abs(x))
+        str_2 = str_1[::-1]
+        if str_1 == str_2:
+            return True
+        return False
